7f49698e170ccc025ea7697c3c4b21a7b76b13aa
MaJunghan <wjdgksak@naver.com>
2022-12-25 22:27:41 +0900

    기본문법배우기/변수,매개변수,리턴값 타입붙이기

---
+++ first.ts
@@ -0,0 +1,41 @@
+// 자바스크립트에 변수, 매개변수, 리턴값만 타입붙인것.
+
+const a: string = "5";
+const b: number = 5;
+const c: boolean = false;
+const d: undefined = undefined;
+const e: null = null;
+const f: any = 5; // javascript
+
+// 함수형
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+
+// 화살표함수
+// type Add = (x: number, y: number) => number;
+// const add: Add = (x, y) => x + y;
+
+// 인터페이스
+interface Add {
+  (x: number, y: number): number;
+}
+const add: Add = (x, y) => x + y;
+
+// 객체
+const obj: { lat: number; lon: number } = {
+  lat: 37.5,
+  lon: 127.5,
+};
+
+// 배열
+const arr: string[] = ["123", "456"];
+const arr2: Array<number> = [123, 456];
+
+// 튜플 : 자료형이 섞여있고, 길이가 고정
+const arr3: [number, number, string] = [1, 2, "3"];
+
+// 타입에 원시값을 적용할 수 있음.
+// const는 상수라 바뀔것이 없어서, 최대한 정확하게!
+const ex1: true = true;
+const ex2: 5 = 5;

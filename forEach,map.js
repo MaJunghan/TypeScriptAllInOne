c47a46137e0158f7bbf54faab899aa79c67b253f
MaJunghan <wjdgksak@naver.com>
2023-01-10 13:45:09 +0900

    filter

--- forEach,map.js
+++ forEach,map.js
@@ -1,3 +1,7 @@
 var eachA = [1, 2, 3, 4];
 eachA.forEach(function (item) { return console.log(item); });
-var mapA = [1, 2, 3].map(function (item) { return item + 1; });
+var mapA = [1, 2, 3];
+mapA.map(function (item) { return item + 1; });
+// filter는 type이 2개라서 추론을 제대로 못하는 경우도 있음,
+var filterA = [1, 2, 3, 4];
+filterA.filter(function (item) { return item % 2 === 1; });
--- forEach,map.ts
+++ forEach,map.ts
@@ -1,10 +1,7 @@
 interface Array<T> {
   forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
   map<U>(callbackfn: (value: number, index: number, array: number[]) => number, thisArg?: any): number[];
-  filter<S extends number>(
-    predicate: (value: number, index: number, array: number[]) => value is number,
-    thisArg?: any
-  ): S[];
+  filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
   filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[];
 }
 
@@ -15,5 +12,8 @@ const mapA: Array<number> = [1, 2, 3];
 mapA.map((item) => item + 1);
 
 // filter는 type이 2개라서 추론을 제대로 못하는 경우도 있음,
-const filterA: Array<number> = [1, 2, 3, 4];
-filterA.filter((item) => item % 2 === 1);
+// 결과가 ['2','4'] string인데 타입추론을 제대로 못함.
+// is로 타입좁히기
+const filterA: Array<number | string> = [1, "2", 3, "4"];
+const predicate = (value: number | string): value is string => typeof value === "string";
+const filters = filterA.filter(predicate);

0dc342da133afed17386b14cd1cd7cbd4b9b1af8
MaJungHan <wjdgksak@naver.com>
2023-01-10 12:52:32 +0900

    임시

--- forEach,map.ts
+++ forEach,map.ts
@@ -1,6 +1,11 @@
 interface Array<T> {
   forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
   map<U>(callbackfn: (value: number, index: number, array: number[]) => number, thisArg?: any): number[];
+  filter<S extends number>(
+    predicate: (value: number, index: number, array: number[]) => value is number,
+    thisArg?: any
+  ): S[];
+  filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[];
 }
 
 const eachA: Array<number> = [1, 2, 3, 4];
@@ -8,3 +13,7 @@ eachA.forEach((item) => console.log(item));
 
 const mapA: Array<number> = [1, 2, 3];
 mapA.map((item) => item + 1);
+
+// filter는 type이 2개라서 추론을 제대로 못하는 경우도 있음,
+const filterA: Array<number> = [1, 2, 3, 4];
+filterA.filter((item) => item % 2 === 1);
